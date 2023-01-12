0e8b41f9e4ab84cbaddeac606c024f598851a64f
dengjiawen <dengjiawen@bilibili.com>
2023-01-12 16:59:31 +0800

    feat: Update cmd

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"regexp"
 )
 
@@ -20,6 +21,12 @@ func main() {
 
 	// 获取输入的第一个参数作为文件名
 	filename := os.Args[1]
+
+	// 更新
+	if filename == "update" {
+		update()
+		return
+	}
 
 	// 读取文件然后使用正则表达式替换
 	bs, err := ioutil.ReadFile(filename)
@@ -41,6 +48,23 @@ func main() {
 
 	// 打印成功消息
 	fmt.Printf("Success! New file: %s", newFileName)
+}
+
+// 自动更新软件
+// 调用命令行 go install github.com/dengjiawen8955/pr@latest
+func update() {
+	// 执行命令
+	cmd := exec.Command("go", "install", "github.com/dengjiawen8955/pr@latest")
+	cmd.Stdout = os.Stdout
+	fmt.Printf("cmd: %v\n", cmd.String())
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Run failed: %v\n", err)
+		return
+	}
+
+	// 打印成功消息
+	fmt.Println("Update success!")
 }
 
 func replace(data string) string {
