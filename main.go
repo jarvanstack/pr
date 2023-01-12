d65ff157be5d098fa7b311f625d15d0509a31c76
dengjiawen <dengjiawen@bilibili.com>
2023-01-12 16:15:39 +0800

    feat: Remote Space

--- main.go
+++ main.go
@@ -42,51 +42,51 @@ func main() {
 
 func replace(data string) string {
 	// 句号
-	re := regexp.MustCompile(`([\p{Han}])\. `)
+	re := regexp.MustCompile(`([\p{Han}])\.[ ]*`)
 	output := re.ReplaceAllString(data, `$1。`)
 
 	// 逗号
-	re = regexp.MustCompile(`([\p{Han}]), `)
+	re = regexp.MustCompile(`([\p{Han}]),[ ]*`)
 	output = re.ReplaceAllString(output, `$1，`)
 
 	// 冒号
-	re = regexp.MustCompile(`([\p{Han}]): `)
+	re = regexp.MustCompile(`([\p{Han}]):[ ]*`)
 	output = re.ReplaceAllString(output, `$1：`)
 
 	// 分号
-	re = regexp.MustCompile(`([\p{Han}]); `)
+	re = regexp.MustCompile(`([\p{Han}]);[ ]*`)
 	output = re.ReplaceAllString(output, `$1；`)
 
 	// 问号
-	re = regexp.MustCompile(`([\p{Han}])\? `)
+	re = regexp.MustCompile(`([\p{Han}])\?[ ]*`)
 	output = re.ReplaceAllString(output, `$1？`)
 
 	// 感叹号
-	re = regexp.MustCompile(`([\p{Han}])! `)
+	re = regexp.MustCompile(`([\p{Han}])![ ]*`)
 	output = re.ReplaceAllString(output, `$1！`)
 
 	// 左括号
-	re = regexp.MustCompile(`\(([\p{Han}])`)
+	re = regexp.MustCompile(`[ ]*\(([\p{Han}])`)
 	output = re.ReplaceAllString(output, `（$1`)
 
 	// 右括号
-	re = regexp.MustCompile(`([\p{Han}])\)`)
+	re = regexp.MustCompile(`([\p{Han}])\)[ ]*`)
 	output = re.ReplaceAllString(output, `$1）`)
 
 	// 左引号
-	re = regexp.MustCompile(`"([\p{Han}])`)
+	re = regexp.MustCompile(`[ ]*"([\p{Han}])`)
 	output = re.ReplaceAllString(output, `“$1`)
 
 	// 右引号
-	re = regexp.MustCompile(`([\p{Han}])"`)
+	re = regexp.MustCompile(`([\p{Han}])"[ ]*`)
 	output = re.ReplaceAllString(output, `$1”`)
 
 	// 左小引号
-	re = regexp.MustCompile(`'([\p{Han}])`)
+	re = regexp.MustCompile(`[ ]*'([\p{Han}])`)
 	output = re.ReplaceAllString(output, `‘$1`)
 
 	// 右小引号
-	re = regexp.MustCompile(`([\p{Han}])'`)
+	re = regexp.MustCompile(`([\p{Han}])'[ ]*`)
 	output = re.ReplaceAllString(output, `$1’`)
 
 	return output
